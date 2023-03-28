732eb557dfd8ce91595001b7276583a2c4df507b
Rafal Kaczmarek <Kaczmarek.rr@gmail.com>
2023-03-28 10:36:14 +0200

    finished ex with all of the results

--- 03-21/ex_02.py
+++ 03-21/ex_02.py
@@ -13,21 +13,29 @@ import os
 from tqdm import tqdm
 from scipy.signal import argrelextrema
 
-# constants
 
 
-N = 100
 ##########################
-def step(u, v):
+
+
+def make_gif(path,name):
+    filenames = os.listdir(path)
+    images = []
+    for filename in tqdm(filenames):
+        images.append(imageio.v2.imread(path + filename))
+
+    kargs = {"duration": 0.2}
+    imageio.mimsave(f"{name}.gif", images, **kargs)
+
+
+def step(u, v,F,k):
     D_u = 2 * 10 ** (-5)
     D_v = 1 * 10 ** (-5)
-    F = 0.025
-    k = 0.055
     dt = 1
     dx = 0.02
 
     u_2nd = (np.roll(u, -1,axis=0) + np.roll(u, +1,axis=0) + np.roll(u, -1,axis=1) + np.roll(u, +1,axis=1) - 4 * u) / dx**2
-    v_2nd = (np.roll(u, -1,axis=0) + np.roll(u, +1,axis=0) + np.roll(u, -1,axis=1) + np.roll(u, +1,axis=1) - 4 * u) / dx**2
+    v_2nd = (np.roll(v, -1,axis=0) + np.roll(v, +1,axis=0) + np.roll(v, -1,axis=1) + np.roll(v, +1,axis=1) - 4 * v) / dx**2
 
 
     u_derivative = D_u * u_2nd - u * v**2 + F * (1 - u)
@@ -39,59 +47,55 @@ def step(u, v):
 
 
 def main():
-    U = np.ones((N,N))
-    V = np.zeros((N,N))
-    X,Y = np.mgrid[0:N, 0:N]
-    X = X*0.02
-    Y = Y*0.02
-    for i in range(int(N / 4), int(3 * N / 4)):
-        for j in range(int(N / 4), int(3 * N / 4)):
-            U[i,j] = np.random.random() * 0.2 + 0.4
-            V[i,j] = np.random.random() * 0.2 + 0.2
+    # constants
+    F = [0.025,0.03,0.01,0.04,0.06,0.037]
+    k = [0.055,0.062,0.047,0.07,0.0615,0.06]
+    N = 100 
 
-    maxims_v = np.zeros((5000, N))
-    for time_step in tqdm(range(100_000)):
-        #where_max = argrelextrema(v, np.greater)
-        #maxims_v[time_step,where_max] = 1
-        U, V = step(U, V)
-        if time_step % 100 == 0:
+    for i, f_i in enumerate(F):
+        k_i = k[i]
+        U = np.ones((N,N))
+        V = np.zeros((N,N))
+        for i in range(int(N / 4), int(3 * N / 4)):
+            for j in range(int(N / 4), int(3 * N / 4)):
+                U[i,j] = np.random.random() * 0.2 + 0.4
+                V[i,j] = np.random.random() * 0.2 + 0.2
 
-            plt.figure()
-            plt.title(time_step)
-            plt.contourf(np.arange(0,100),np.arange(0,100),V)
+        for time_step in tqdm(range(10_000)):
+            #where_max = argrelextrema(v, np.greater)
+            #maxims_v[time_step,where_max] = 1
+            U, V = step(U, V,f_i,k_i)
+            if time_step % 200 == 0:
 
-            if time_step < 10:
-                title = "0000" + str(time_step)
+                plt.figure()
+                plt.title(time_step)
+                plt.contourf(np.arange(100),np.arange(100),V)
 
-            elif time_step < 100:
-                title = "000" + str(time_step)
-            elif time_step < 1000:
-                title = "00" + str(time_step)
-            elif time_step < 10000:
-                title = "0" + str(time_step)
-            else:
-                title = str(time_step)
-            plt.savefig(f"results_2/{title}.png")
-            plt.close()
+                if time_step < 10:
+                    title = "0000" + str(time_step)
 
+                elif time_step < 100:
+                    title = "000" + str(time_step)
+                elif time_step < 1000:
+                    title = "00" + str(time_step)
+                elif time_step < 10000:
+                    title = "0" + str(time_step)
+                else:
+                    title = str(time_step)
+                plt.savefig(f"results_2/{title}.png")
+                plt.close()
+        make_gif("results_2/",f"movie_{f_i}-{k_i}")
     #print(where_max)
     #plt.figure()
     #plt.imshow(maxims_v, aspect=0.008, vmax=1, vmin=0,origin="lower")
     #plt.savefig("maxims_v_2.png")
 
 
-def make_gif(path):
-    filenames = os.listdir(path)
-    images = []
-    for filename in tqdm(filenames):
-        images.append(imageio.v2.imread(path + filename))
 
-    kargs = {"duration": 0.2}
-    imageio.mimsave("movie_2.gif", images, **kargs)
 
 
 if __name__ == "__main__":
 
     
     main()
-    make_gif("results_2/")
+    
